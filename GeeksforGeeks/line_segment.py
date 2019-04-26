2e0a1995a72c788370ab008e97233f9b339d0177
Tejesh <tejeshreddy111@gmail.com>
2019-04-26 11:55:53 +0530

    Added line_segment program

---
+++ GeeksforGeeks/line_segment.py
@@ -0,0 +1,10 @@
+#https://practice.geeksforgeeks.org/problems/check-if-two-line-segments-intersect/0
+
+
+
+
+if __name__ == '__main__':
+    test_case=int(input())
+    while test_case:
+        
+        test_case-=1
